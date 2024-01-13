ed13840ac7511225ed84b60179304d35506c3e2b
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-01-13 04:07:20 -0800

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a readable string representation
